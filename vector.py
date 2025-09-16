a1d62fceebcd64580ef1f1fc12e1859c7653c103
Omar Nassar <77386411+iidromar@users.noreply.github.com>
2025-09-16 17:10:39 +0300

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,3 +1,4 @@
+
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
@@ -5,24 +6,13 @@ import os
 import pandas as pand
 
 df = pand.read_csv("Store_reviews.csv")
+
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
 db_location = "./chroma_langchain_db"
+
 add_documents = not os.path.exists(db_location)
 
-if add_documents:
-    documents = []
-    ids = []
-    
-    for i, row in df.iterrows():
-        document = Document(
-            page_content=row["Title"] + " " + row["Review"],
-            metadata={"rating": row["Rating"], "date": row["Date"]},
-            id=str(i)
-        )
-        ids.append(str(i))
-        documents.append(document)
-        
 vector_store = Chroma(
     collection_name="restaurant_reviews",
     persist_directory=db_location,
@@ -30,8 +20,25 @@ vector_store = Chroma(
 )
 
 if add_documents:
-    vector_store.add_documents(documents=documents, ids=ids)
+    print("Creating new vector database and adding documents...")
+    documents = []
+    ids = []
     
+    for i, row in df.iterrows():
+        page_content = str(row["Title"]) + " " + str(row["Review"])
+        
+        document = Document(
+            page_content=page_content,
+            metadata={"rating": row["Rating"], "date": row["Date"]},
+        )
+        ids.append(str(i))
+        documents.append(document)
+        
+    vector_store.add_documents(documents=documents, ids=ids)
+    print("Documents added successfully.")
+
 retriever = vector_store.as_retriever(
-    search_kwargs={"k": 5}
+    search_kwargs={"k": 5} 
 )
+
+print("Retriever is ready.")
